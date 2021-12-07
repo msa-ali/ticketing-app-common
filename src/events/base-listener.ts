5b03493731752de3761c779d807c595ffa771bc1
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2021-12-07 10:14:02 +0530

    updates

---
+++ src/events/base-listener.ts
@@ -0,0 +1,54 @@
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+abstract class Listener<T extends Event> {
+    abstract subject: T['subject'];
+    abstract queueGroupName: string;
+    abstract onMessage(data: T['data'], msg: Message): void;
+    private client: Stan;
+    protected ackWait = 5 * 1000;
+
+    constructor(client: Stan) {
+        this.client = client;
+    }
+
+    subscriptionOptions() {
+        return this.client
+            .subscriptionOptions()
+            .setDeliverAllAvailable()
+            .setManualAckMode(true)
+            .setAckWait(this.ackWait)
+            .setDurableName(this.queueGroupName);
+    }
+
+    listen() {
+        const susbcription = this.client.subscribe(
+            this.subject,
+            this.queueGroupName,
+            this.subscriptionOptions(),
+        );
+
+        susbcription.on('message', (msg: Message) => {
+            console.log(
+                `Message Received: ${this.subject} / ${this.queueGroupName}`
+            );
+            const parsedMessage = this.parseMessage(msg);
+            this.onMessage(parsedMessage, msg);
+
+        })
+    }
+
+    parseMessage(msg: Message) {
+        const data = msg.getData();
+        return typeof data === 'string'
+            ? JSON.parse(data)
+            : JSON.parse(data.toString('utf-8'));
+    }
+}
+
+export default Listener;
